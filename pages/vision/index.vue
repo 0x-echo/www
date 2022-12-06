26cc628c87dacb903fbc4c8b42cbe8324046d75f
QiongrongJiang <qiongrongjiang@gmail.com>
2022-12-05 21:18:37 -0500

    update vision

--- pages/vision/index.vue
+++ pages/vision/index.vue
@@ -1,10 +1,17 @@
 <template>
   <template-doc
+    class="vision-page"
     subtitle=""
     title="Vision">
     <h2>Long live our opinion.</h2> 
-    <br>
-    Opinion should be a part of things. Our vision is building a decentralized platform to help collecting, saving and displaying opinions through time and space.
+    
+    <p>
+      ECHO is the first decentralized social engagement protocol based on Arweave. Our goal is to provide the fundamental infrastructure of Web3 social by introducing the first comment widget that can be deployed on any Web3 website with permanent data storage, so that users can speak up for themselves in a decentralized, permissionless, and censorship-resistant environment. It is our conviction that every person deserves to be heard, and we strive to create the right tool to make that happen. “Long live our opinion”, as our slogan goes.
+    </p>
+    
+    <p>
+      More importantly, as we are providing a multi-chain, multi-platform, and multi-disciplinary solution, ECHO will energize the social mobility in an aggregated manner to address the current social segregation issue in Web3. And our protocol-based data structure of social activities both on-chain and off-chain will be the cornerstone of the future Web3 social paradigm.
+    </p>
   </template-doc>
 </template>
 
@@ -15,5 +22,10 @@ useHead({
 </script>
 
 <style lang="scss">
-
+.vision-page {
+  h2,
+  p {
+    margin-bottom: 20px;
+  }
+}
 </style>
