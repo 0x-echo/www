842da015c727523794d9e508f2ebc4f500df22fb
Airyland <i@mao.li>
2022-12-12 16:50:51 -0500

    add changelog

--- pages/changelog/index.vue
+++ pages/changelog/index.vue
@@ -14,6 +14,36 @@
 import ChangelogItem from './components/changelog-item'
 
 const logs = [{
+  title: 'Protocol Update',
+  date: 'December 12, 2022',
+  sections: [{
+    label: 'Feature',
+    type: 'success',
+    list: [{
+      content: 'Add field: from_uri to mark where comment is from'
+    }]
+  }]
+}, {
+  title: 'ECHO Extension',
+  date: 'December 10, 2022',
+  sections: [{
+    label: 'Feature',
+    type: 'success',
+    list: [{
+      content: 'The ECHO Chrome extension is available now.'
+    }]
+  }]
+}, {
+  title: 'ECHO Button',
+  date: 'December 4, 2022',
+  sections: [{
+    label: 'Feature',
+    type: 'success',
+    list: [{
+      content: 'The Button JS SDK has been released.'
+    }]
+  }]
+}, {
   title: 'Markdown is Supported!',
   date: 'August 13, 2022',
   sections: [{
